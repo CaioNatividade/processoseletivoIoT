e8e6d841395d02fb03deba5f69b03fc9c0719e60
CaioNatividade <caioprofissional22@gmail.com>
2026-07-26 20:24:55 +0000

    fix: utiliza saida digital do LDR para detectar peças

--- src/main.py
+++ src/main.py
@@ -1,18 +1,15 @@
-from machine import ADC, Pin
+from machine import Pin
 from time import ticks_diff, ticks_ms, sleep_ms
 
 
 PINO_LDR = 34
 PINO_BOTAO = 27
 
-LIMITE_BLOQUEIO = 100
 TEMPO_MICRO_PARADA_MS = 5000
 TEMPO_DEBOUNCE_MS = 50
 
 
-ldr = ADC(Pin(PINO_LDR))
-ldr.atten(ADC.ATTN_11DB)
-ldr.width(ADC.WIDTH_12BIT)
+ldr = Pin(PINO_LDR, Pin.IN)
 
 botao = Pin(PINO_BOTAO, Pin.IN, Pin.PULL_UP)
 
@@ -23,25 +20,19 @@ objeto_bloqueando = False
 inicio_bloqueio = None
 alerta_micro_parada_emitido = False
 
-ultimo_estado_botao = botao.value()
-ultimo_instante_mudanca_botao = ticks_ms()
+ultimo_estado_lido_botao = botao.value()
+estado_estavel_botao = ultimo_estado_lido_botao
+instante_mudanca_botao = ticks_ms()
 botao_processado = False
 
 
 print("Contador de Producao Inicializado")
 
 
-def converter_adc_para_lux(valor_adc):
-    return int((valor_adc / 4095) * 1000)
-
-
 while True:
     agora = ticks_ms()
 
-    valor_adc = ldr.read()
-    lux = converter_adc_para_lux(valor_adc)
-
-    bloqueado = lux < LIMITE_BLOQUEIO
+    bloqueado = ldr.value() == 1
 
     if bloqueado and not objeto_bloqueando:
         objeto_bloqueando = True
@@ -68,23 +59,26 @@ while True:
 
         alerta_micro_parada_emitido = False
 
-    estado_atual_botao = botao.value()
+    leitura_botao = botao.value()
 
-    if estado_atual_botao != ultimo_estado_botao:
-        ultimo_estado_botao = estado_atual_botao
-        ultimo_instante_mudanca_botao = agora
+    if leitura_botao != ultimo_estado_lido_botao:
+        ultimo_estado_lido_botao = leitura_botao
+        instante_mudanca_botao = agora
 
-    if ticks_diff(agora, ultimo_instante_mudanca_botao) >= TEMPO_DEBOUNCE_MS:
-        if estado_atual_botao == 0 and not botao_processado:
-            total_pecas = 0
-            objeto_bloqueando = False
-            inicio_bloqueio = None
-            alerta_micro_parada_emitido = False
+    if ticks_diff(agora, instante_mudanca_botao) >= TEMPO_DEBOUNCE_MS:
+        if leitura_botao != estado_estavel_botao:
+            estado_estavel_botao = leitura_botao
 
-            print("Turno resetado com sucesso. Contadores zerados.")
-            botao_processado = True
+            if estado_estavel_botao == 0 and not botao_processado:
+                total_pecas = 0
+                objeto_bloqueando = False
+                inicio_bloqueio = None
+                alerta_micro_parada_emitido = False
 
-        elif estado_atual_botao == 1:
-            botao_processado = False
+                print("Turno resetado com sucesso. Contadores zerados.")
+                botao_processado = True
 
-    sleep_ms(20)
+            elif estado_estavel_botao == 1:
+                botao_processado = False
+
+    sleep_ms(10)
